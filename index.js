d69ccab3a2d1482ccb63bab34af0cf9716d324bc
Thibaut Rey <thibautrey@me.com>
2017-03-29 19:43:10 +0200

    - Log requests on web hook

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/webhook', function(req, res) {
 
 app.post('/webhook', function(req, res){
     var data = req.body;
-
+    console.log(data);
     // Make sure this is a page subscription
     if(data.object === 'page'){
         // Iterate over each entries - there may be multiple if batched
