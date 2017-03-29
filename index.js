62686dd5060a03ad1e1635c6cac5624356819c46
Thibaut Rey <thibautrey@me.com>
2017-03-29 19:14:18 +0200

    - Update web hook implementation to pass Facebook test

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ app.get('/', function(request, response) {
     response.send('Hello World!')
 });
 
-app.get('/webhook', function(request, response) {
-    response.send('Messenger webhook')
+app.get('/webhook', function(req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
 });
 
 app.listen(app.get('port'), function() {
