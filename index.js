8c713361a4d654aff51763a2985f4894dfb8bb08
Thibaut Rey <thibautrey@me.com>
2017-03-29 19:32:03 +0200

    - Update verify web hook

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ app.get('/webhook', function(req, res) {
         });
 
         res.sendStatus(200);
+    }else{
+        if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === "QYKLKbipwgVXYTuwXMQxwq7uqz") {
+            console.log("Validating webhook");
+            res.status(200).send(req.query['hub.challenge']);
+        } else {
+            console.error("Failed validation. Make sure the validation tokens match.");
+            res.sendStatus(403);
+        }
     }
 });
 
