a7ba8c0c538e6119d77fbc32e0fcb831292d921c
Thibaut Rey <thibautrey@Thibauts-MacBook-Pro.local>
2017-03-29 18:58:06 +0200

    Add test skeleton

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var expect    = require("chai").expect;
+
+describe("Index.js", function() {
+    describe("webhook", function(){
+        it("should accept requests", function(){
+            expect(1).to.equal(1);
+        })
+    });
+});
